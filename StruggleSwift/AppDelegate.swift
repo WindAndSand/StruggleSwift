ba8fc32f17446d4ebb939e59979b531275378ebb
WindAndSand <2711722020@qq.com>
2017-08-11 10:35:32 +0800

    创建 RootViewContoller

--- StruggleSwift/AppDelegate.swift
+++ StruggleSwift/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let rootVC = RootViewController()
+        
+        let nav = UINavigationController.init(rootViewController: rootVC)
+        
+        self.window?.rootViewController = nav
+        self.window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
