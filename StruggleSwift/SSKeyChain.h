5d14fa3a6fe60182e5e3246681e3319b7b867191
WindAndSand <2711722020@qq.com>
2017-12-24 17:23:18 +0800

    KeyChain的学习，但是用swift写有问题

---
+++ StruggleSwift/SSKeyChain.h
@@ -0,0 +1,13 @@
+//
+//  SSKeyChain.h
+//  StruggleSwift
+//
+//  Created by 黄刚 on 2017/12/24.
+//  Copyright © 2017年 HuangGang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSKeyChain : NSObject
+
+@end
---
+++ StruggleSwift/SSKeyChain.m
@@ -0,0 +1,13 @@
+//
+//  SSKeyChain.m
+//  StruggleSwift
+//
+//  Created by 黄刚 on 2017/12/24.
+//  Copyright © 2017年 HuangGang. All rights reserved.
+//
+
+#import "SSKeyChain.h"
+
+@implementation SSKeyChain
+
+@end
