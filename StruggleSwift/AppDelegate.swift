f5c55988aa55c977224db041820350a118cb43f5
harely <harley@yunio.com>
2018-03-26 10:25:11 +0800

    添加app的icon

--- StruggleSwift/AppDelegate.swift
+++ StruggleSwift/AppDelegate.swift
@@ -32,9 +32,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        
         self.visualEffect.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
         self.visualEffect.alpha = 0.7
         application.keyWindow?.addSubview(visualEffect)
